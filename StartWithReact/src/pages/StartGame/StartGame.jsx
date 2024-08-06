e760561a715461fc5b0f0f3c73ae8b12b93f47e0
Shubham Bhardwaj <shubhambhardwaj@Shubhams-Air.bbrouter>
2024-08-06 21:39:02 +0530

    suggested from discord communiti

--- StartWithReact/src/pages/StartGame/StartGame.jsx
+++ StartWithReact/src/pages/StartGame/StartGame.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function StartGame(){
     return (
         <>
-        <Link to={'"/StartGame"'}>
+        <Link to='/StartGame'>
         Start the Game
         </Link>
         </>
